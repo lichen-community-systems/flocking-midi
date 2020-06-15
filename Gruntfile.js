d7cbb27730320f941acc87cca0209d867403089f
Colin Clark <colinbdclark@gmail.com>
2020-06-15 13:45:09 -0400

    flocking-midi/gh-1, flocking-midi/gh-2: Fixes testem and linting.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function (grunt) {
     grunt.initConfig({
         lintAll: {
             sources: {
-                md:    [ "./*.md","./docs/**/*.md"],
+                md:    [ "./*.md"],
                 js:    ["./src/**/*.js", "./tests/**/*.js", "./*.js"],
                 json:  ["./src/**/*.json", "tests/**/*.json", "./*.json"],
                 json5: ["./src/**/*.json5", "tests/**/*.json5", "./*.json5"],
@@ -17,5 +17,4 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks("gpii-grunt-lint-all");
     grunt.registerTask("lint", "Perform all standard lint checks.", ["lint-all"]);
     grunt.registerTask("default", ["lint"]);
-
 };
